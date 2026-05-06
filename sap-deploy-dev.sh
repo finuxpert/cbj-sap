d3e3a8ba4c79c654866dfe7b2096f881ee509007
Zactos-lab <fikrirtc12@gmail.com>
2026-05-07 01:13:33 +0700

    Point sapdev deploy helper to nginx root

--- sap-deploy-dev.sh
+++ sap-deploy-dev.sh
@@ -4,11 +4,14 @@
 # Usage:
 #   sudo ./sap-deploy-dev.sh
 #
+# Nginx sapdev root saat ini:
+#   /var/www/svr01-dev/sap
+#
 # Default:
 #   BASE_PATH=/
-#   DEPLOY_DIR=/var/www/html/sapdev
+#   DEPLOY_DIR=/var/www/svr01-dev/sap
 #
-# Override if nginx root berbeda:
+# Override jika nginx root berubah:
 #   sudo DEPLOY_DIR=/path/to/sapdev ./sap-deploy-dev.sh
 
 set -Eeuo pipefail
@@ -16,7 +19,7 @@ set -Eeuo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export BASE_PATH="${BASE_PATH:-/}"
-export DEPLOY_DIR="${DEPLOY_DIR:-/var/www/html/sapdev}"
+export DEPLOY_DIR="${DEPLOY_DIR:-/var/www/svr01-dev/sap}"
 export DO_BACKUP="${DO_BACKUP:-0}"
 
 exec "${SCRIPT_DIR}/sap-deploy.sh" "$@"
