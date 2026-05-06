f6035c1ce41763922cc4225c36175490d13b78d9
Zactos-lab <fikrirtc12@gmail.com>
2026-05-07 01:01:07 +0700

    Load premium sapdev overhaul stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './sapdev-polish.css'
 import './sapdev-comparer-fix.css'
+import './sapdev-premium-overhaul.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
