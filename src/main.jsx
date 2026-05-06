a6ca3a51e6648ff437f53eeaf52b20c8ee4d9385
Zactos-lab <fikrirtc12@gmail.com>
2026-05-07 02:30:20 +0700

    Rescue app by disabling risky runtime enhancers

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import './sapdev-premium-overhaul.css'
 import './sapdev-final-force.css'
 import './comparer-nav-style-override.js'
 import './comparer-rca-final.css'
-import './comparer-rca-dashboard-enhancer.js'
-import './sap-zip-reader.js'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
