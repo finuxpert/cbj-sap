4c42d033fd3971e5991aaea2a163907be6303ef8
Zactos-lab <fikrirtc12@gmail.com>
2026-05-07 01:54:27 +0700

    Load final comparer RCA CSS override

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './sapdev-comparer-fix.css'
 import './sapdev-premium-overhaul.css'
 import './sapdev-final-force.css'
 import './comparer-nav-style-override.js'
+import './comparer-rca-final.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
