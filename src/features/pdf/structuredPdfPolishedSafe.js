612a7a46b1b4b51b20c3a6a1c2d5ea21b4919aa9
Zactos-lab <fikrirtc12@gmail.com>
2026-05-13 06:15:11 +0700

    Switch comparer export to WP-SCOUT enterprise V4 PDF

--- src/features/pdf/structuredPdfPolishedSafe.js
+++ src/features/pdf/structuredPdfPolishedSafe.js
@@ -1,5 +1,5 @@
 import { exportStructuredPdf as exportPolishedStructuredPdf } from './structuredPdfPolished.js'
-import { exportWpScoutVisualPdf } from './wpScoutVisualPdfEnterpriseV3.js'
+import { exportWpScoutVisualPdf } from './wpScoutVisualPdfEnterpriseV4.js'
 
 export async function exportStructuredPdf(slug) {
   if (slug === 'comparer') {
