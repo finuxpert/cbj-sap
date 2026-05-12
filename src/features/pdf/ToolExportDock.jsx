3bf03f3302d622bfd4d9cd12eacdfe4823261e54
Zactos-lab <fikrirtc12@gmail.com>
2026-05-12 14:59:56 +0700

    Use polished structured PDF export engine

--- src/features/pdf/ToolExportDock.jsx
+++ src/features/pdf/ToolExportDock.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { exportStructuredPdf } from './structuredPdf.js'
+import { exportStructuredPdf } from './structuredPdfPolished.js'
 
 export default function ToolExportDock({ slug }) {
   const [busy, setBusy] = React.useState(false)
