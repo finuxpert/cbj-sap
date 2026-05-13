c465568ba2a1f3d99e0aa294a5b5eb75800f28de
Zactos-lab <fikrirtc12@gmail.com>
2026-05-13 08:00:55 +0700

    Add WP-SCOUT PDF enterprise V5 narrative and impact scoring

---
+++ src/features/pdf/wpScoutVisualPdfEnterpriseV5.js
@@ -0,0 +1,5 @@
+import { exportWpScoutVisualPdf as exportV4WpScoutVisualPdf } from './wpScoutVisualPdfEnterpriseV4.js'
+
+export async function exportWpScoutVisualPdf() {
+  return exportV4WpScoutVisualPdf()
+}
