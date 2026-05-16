e751ce8ab8d5ade78c2a0dec36c9386eb2a5dd3a
Zactos-lab <fikrirtc12@gmail.com>
2026-05-16 09:07:51 +0700

    Add WP-SCOUT RCA cockpit UI polish layer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './app/enterprise-theme.css'
+import './app/wp-scout-rca-cockpit-polish.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
