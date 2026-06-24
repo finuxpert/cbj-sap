1f704553377f430f1fd6e6000756ab0e6a07cc2e
Zactos-lab <fikrirtc12@gmail.com>
2026-06-24 22:50:02 +0700

    Disable problematic ST03N responsive override

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import './app/rca-workspace.css'
 import './app/rca-final-ui.css'
 import './app/rca-log-ui.css'
-import './app/st03n-responsive-ui.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
