92944d5b7115d31522907e39ce4d744022c95e40
Zactos-lab <fikrirtc12@gmail.com>
2026-06-24 15:45:24 +0700

    Load Log RCA UI polish stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import './app/rca-workspace.css'
 import './app/rca-final-ui.css'
+import './app/rca-log-ui.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
