5074b47832c4e568e9e523d0ed0c00c9252e86ec
Zactos-lab <fikrirtc12@gmail.com>
2026-06-24 15:11:20 +0700

    Polish SAP RCA final UI entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './app/rca-workspace.css'
+import './app/rca-final-ui.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
