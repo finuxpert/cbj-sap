7bbe99a0aa619a234ff8abb74caeb3072c3fe700
Zactos-lab <fikrirtc12@gmail.com>
2026-07-08 09:52:19 +0700

    Stop runtime RCA mockup override installers

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,13 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import { tools } from './tools'
 import useRoute from './app/useRoute.js'
-import { installEnterpriseRcaMockup } from './tools/installEnterpriseRcaMockup.js'
-import { installEnterpriseRcaPhase2 } from './tools/installEnterpriseRcaPhase2.js'
-import { installEnterpriseRcaPhase3 } from './tools/installEnterpriseRcaPhase3.js'
-import { installEnterpriseRcaPhase4 } from './tools/installEnterpriseRcaPhase4.js'
-import { installEnterpriseRcaPhase5 } from './tools/installEnterpriseRcaPhase5.js'
-import './tools/EnterpriseRcaMockup.css'
 
-const APP_BUILD_STAMP = 'sap-20260708-compact-rca-phase5'
+const APP_BUILD_STAMP = 'sap-20260708-final-rca-dashboard'
 
 export default function App() {
   const route = useRoute()
   const tool = tools.find((item) => item.slug === route.name) || tools[0]
   const ActiveTool = tool.Component
-
-  React.useEffect(() => {
-    installEnterpriseRcaMockup()
-    installEnterpriseRcaPhase2()
-    installEnterpriseRcaPhase3()
-    installEnterpriseRcaPhase4()
-    installEnterpriseRcaPhase5()
-  }, [route.name])
 
   return (
     <div className="appShell isTool compactRcaApp" data-build={APP_BUILD_STAMP}>
