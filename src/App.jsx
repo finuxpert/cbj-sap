8e6e74a40d58f9e004c5558395892e705a3cfaa4
Zactos-lab <fikrirtc12@gmail.com>
2026-07-08 09:18:11 +0700

    Install compact RCA mockup runtime stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import { tools } from './tools'
 import useRoute from './app/useRoute.js'
+import { installEnterpriseRcaMockup } from './tools/installEnterpriseRcaMockup.js'
 import './tools/EnterpriseRcaMockup.css'
 
-const APP_BUILD_STAMP = 'sap-20260708-compact-rca-mockup'
+const APP_BUILD_STAMP = 'sap-20260708-compact-rca-mockup-runtime'
 
 export default function App() {
   const route = useRoute()
   const tool = tools.find((item) => item.slug === route.name) || tools[0]
   const ActiveTool = tool.Component
+
+  React.useEffect(() => {
+    installEnterpriseRcaMockup()
+  }, [route.name])
 
   return (
     <div className="appShell isTool compactRcaApp" data-build={APP_BUILD_STAMP}>
