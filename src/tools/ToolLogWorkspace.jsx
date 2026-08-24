ec4942a8a9e34e7ddc54733dd6222cc4b29c40bc
Zactos-lab <fikrirtc12@gmail.com>
2026-08-24 22:28:35 +0700

    chore: load LOG v1.5.5 polish

--- src/tools/ToolLogWorkspace.jsx
+++ src/tools/ToolLogWorkspace.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './RcaWorkspaceV153.css'
 import './RcaWorkspaceV154.css'
+import './RcaWorkspaceV155.css'
 
 const LogAnalysis2026 = React.lazy(() => import('./ToolLogAnalysis2026.jsx'))
 
