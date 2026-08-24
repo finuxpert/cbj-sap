77b6b00b1c245110fdb1054d98ffb4cd7c1f0e6c
Zactos-lab <fikrirtc12@gmail.com>
2026-08-24 19:40:51 +0700

    chore: bump release to v1.5.1

--- src/app/version.js
+++ src/app/version.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'SAP RCA Workspace'
-export const APP_VERSION = '1.5.0'
+export const APP_VERSION = '1.5.1'
 
 export function formatAppTitle(section = '') {
   return `${section ? `${section} · ` : ''}${APP_NAME} v${APP_VERSION}`
