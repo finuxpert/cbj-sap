9fcb02a07410994d97b3f56b81d4dd16034d2ef3
Zactos-lab <fikrirtc12@gmail.com>
2026-08-26 16:45:20 +0700

    style: load incident episode workspace overrides

--- src/tools/ToolLogWorkspace.jsx
+++ src/tools/ToolLogWorkspace.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './RcaWorkspaceV153.css'
 import './RcaWorkspaceV154.css'
 import './RcaWorkspaceV155.css'
+import './RcaWorkspaceV156.css'
 
 const LogAnalysis2026 = React.lazy(() => import('./ToolLogAnalysis2026.jsx'))
 
