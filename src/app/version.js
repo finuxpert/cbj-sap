27376e8095e6622e560d6695ad8ff20a1caf889e
Zactos-lab <fikrirtc12@gmail.com>
2026-08-26 18:47:27 +0700

    chore: publish LOG analytics engine v1.2

--- src/app/version.js
+++ src/app/version.js
@@ -1,5 +1,6 @@
 export const APP_NAME = 'SAP RCA Workspace'
 export const APP_VERSION = '1.5.7'
+export const LOG_ANALYTICS_ENGINE = 'deterministic-v1.2'
 
 export function formatAppTitle(section = '') {
   return `${section ? `${section} · ` : ''}${APP_NAME} v${APP_VERSION}`
