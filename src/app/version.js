394b5306578f9cddd74a9550eeea2b28d744c2c0
Zactos-lab <fikrirtc12@gmail.com>
2026-08-27 11:09:23 +0700

    release: bump LOG scope semantics to v1.5.8

--- src/app/version.js
+++ src/app/version.js
@@ -1,5 +1,5 @@
 export const APP_NAME = 'SAP RCA Workspace'
-export const APP_VERSION = '1.5.7'
+export const APP_VERSION = '1.5.8'
 export const LOG_ANALYTICS_ENGINE = 'deterministic-v1.2'
 
 export function formatAppTitle(section = '') {
