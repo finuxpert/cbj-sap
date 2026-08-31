5be5e2ff8e77a60741f2cff4bc769f698ce6f4bd
Zactos-lab <fikrirtc12@gmail.com>
2026-09-01 00:57:24 +0700

    chore: bump app for collector V2.2 ingestion

--- src/app/version.js
+++ src/app/version.js
@@ -1,7 +1,7 @@
 export const APP_NAME = 'SAP RCA Workspace'
-export const APP_VERSION = '1.14.6'
+export const APP_VERSION = '1.15.0'
 export const LOG_ANALYTICS_ENGINE = 'legacy-final-ui-semantics-v3.6.4'
-export const LOG_UI_REVISION = 'standard-mode-ux-cleanup-v3.6.6'
+export const LOG_UI_REVISION = 'collector-v22-ingestion-v3.7.0'
 
 export function formatAppTitle(section = '') {
   return `${section ? `${section} · ` : ''}${APP_NAME} v${APP_VERSION}`
