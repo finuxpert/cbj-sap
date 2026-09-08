85d528dc9ace858cfe00175cdc7f531391748d5b
Zactos-lab <fikrirtc12@gmail.com>
2026-09-08 14:39:37 +0700

    feat: rebrand SAP RCA Workspace to SPHERE

--- src/app/version.js
+++ src/app/version.js
@@ -1,5 +1,6 @@
-export const APP_NAME = 'SAP RCA Workspace'
-export const APP_VERSION = '1.15.0'
+export const APP_NAME = 'SPHERE'
+export const APP_TAGLINE = 'SAP Performance Health Evaluation & Reporting'
+export const APP_VERSION = '1.15.1'
 export const LOG_ANALYTICS_ENGINE = 'legacy-final-ui-semantics-v3.6.4'
 export const LOG_UI_REVISION = 'collector-v22-ingestion-v3.7.0'
 
